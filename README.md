f7c9f18d3536541e2f3e8ae83552c223e7b15de1
SoraKumo <info@croud.jp>
2023-12-29 17:40:09 +0900

    Update dependencies and add linting and formatting

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Deploy the following code to Cloudflare Workers to receive requests for next/ima
 
 ```ts
 import { optimizeImage } from 'wasm-image-optimization';
-export interface Env {}
 
 const isValidUrl = (url: string) => {
 	try {
@@ -15,15 +14,26 @@ const isValidUrl = (url: string) => {
 	}
 };
 
-const handleRequest = async (request: Request, _env: Env, ctx: ExecutionContext): Promise<Response> => {
+const handleRequest = async (request: Request, _env: {}, ctx: ExecutionContext): Promise<Response> => {
+	const accept = request.headers.get('accept');
+	const isWebp =
+		accept
+			?.split(',')
+			.map((format) => format.trim())
+			.some((format) => ['image/webp', '*/*', 'image/*'].includes(format)) ?? true;
+
 	const url = new URL(request.url);
+
 	const params = url.searchParams;
 	const imageUrl = params.get('url');
 	if (!imageUrl || !isValidUrl(imageUrl)) {
 		return new Response('url is required', { status: 400 });
 	}
+
 	const cache = caches.default;
-	const cachedResponse = await cache.match(new Request(url.toString(), request));
+	url.searchParams.append('webp', isWebp.toString());
+	const cacheKey = new Request(url.toString());
+	const cachedResponse = await cache.match(cacheKey);
 	if (cachedResponse) {
 		return cachedResponse;
 	}
@@ -31,25 +41,40 @@ const handleRequest = async (request: Request, _env: Env, ctx: ExecutionContext)
 	const width = params.get('w');
 	const quality = params.get('q');
 
-	const srcImage = await fetch(imageUrl, { cf: { cacheKey: imageUrl } })
-		.then((res) => (res.ok ? res.arrayBuffer() : null))
-		.catch((e) => null);
+	const [srcImage, contentType] = await fetch(imageUrl, { cf: { cacheKey: imageUrl } })
+		.then(async (res) => (res.ok ? ([await res.arrayBuffer(), res.headers.get('content-type')] as const) : []))
+		.catch(() => []);
 
 	if (!srcImage) {
 		return new Response('image not found', { status: 404 });
 	}
+
+	if (contentType && ['image/svg+xml', 'image/gif'].includes(contentType)) {
+		const response = new Response(srcImage, {
+			headers: {
+				'Content-Type': contentType,
+				'Cache-Control': 'public, max-age=31536000, immutable',
+			},
+		});
+		ctx.waitUntil(cache.put(cacheKey, response.clone()));
+		return response;
+	}
+
+	const format = isWebp ? 'webp' : contentType === 'image/jpeg' ? 'jpeg' : 'png';
 	const image = await optimizeImage({
 		image: srcImage,
 		width: width ? parseInt(width) : undefined,
 		quality: quality ? parseInt(quality) : undefined,
+		format,
 	});
 	const response = new Response(image, {
 		headers: {
-			'Content-Type': 'image/webp',
+			'Content-Type': `image/${format}`,
 			'Cache-Control': 'public, max-age=31536000, immutable',
+			date: new Date().toUTCString(),
 		},
 	});
-	ctx.waitUntil(cache.put(request, response.clone()));
+	ctx.waitUntil(cache.put(cacheKey, response.clone()));
 	return response;
 };
 
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,9 @@
-import { unstable_dev } from 'wrangler';
 import { beforeAllAsync } from 'jest-async';
+import { unstable_dev } from 'wrangler';
+
+const images = ['test01.png', 'test02.jpg', 'test03.avif', 'test04.gif'];
+const imageUrl = (image: string) =>
+	`https://raw.githubusercontent.com/SoraKumo001/cloudflare-workers-image-optimization/master/images/${image}`;
 
 describe('Wrangler', () => {
 	const property = beforeAllAsync(async () => {
@@ -21,39 +25,54 @@ describe('Wrangler', () => {
 		expect(res.status).toBe(400);
 		expect(await res.text()).toBe('url is required');
 	});
-
+	test('not found', async () => {
+		const { worker, time } = await property;
+		for (let i = 0; i < images.length; i++) {
+			const url = imageUrl('_' + images[i]);
+			const res = await worker.fetch(`/?url=${encodeURI(url)}&t=${time}`, { headers: { accept: 'image/webp,image/jpeg,image/png' } });
+			expect(res.status).toBe(404);
+		}
+	});
 	test('webp', async () => {
 		const { worker, time } = await property;
-		const url = 'https://raw.githubusercontent.com/node-libraries/wasm-image-optimization/master/images/test01.jpg';
-		const res = await worker.fetch(`/?url=${encodeURI(url)}&t=${time}`, { headers: { accept: 'image/webp,image/jpeg,image/png' } });
-		expect(res.status).toBe(200);
-		expect(Object.fromEntries(res.headers.entries())).toMatchObject({ 'content-type': 'image/webp' });
-		expect(res.headers.get('cf-cache-status')).toBeNull();
-		console.log(Array.from(res.headers.entries()));
+		const types = ['webp', 'webp', 'webp', 'gif'];
+		for (let i = 0; i < images.length; i++) {
+			const url = imageUrl(images[i]);
+			const res = await worker.fetch(`/?url=${encodeURI(url)}&t=${time}`, { headers: { accept: 'image/webp,image/jpeg,image/png' } });
+			expect(res.status).toBe(200);
+			expect(Object.fromEntries(res.headers.entries())).toMatchObject({ 'content-type': `image/${types[i]}` });
+			expect(res.headers.get('cf-cache-status')).toBeNull();
+		}
 	});
 	test('webp(cache)', async () => {
 		const { worker, time } = await property;
-		const url = 'https://raw.githubusercontent.com/node-libraries/wasm-image-optimization/master/images/test01.jpg';
-		const res = await worker.fetch(`/?url=${encodeURI(url)}&t=${time}`, { headers: { accept: 'image/webp,image/jpeg,image/png' } });
-		expect(res.status).toBe(200);
-		expect(Object.fromEntries(res.headers.entries())).toMatchObject({ 'content-type': 'image/webp', 'cf-cache-status': 'HIT' });
-		console.log(Array.from(res.headers.entries()));
+		const types = ['webp', 'webp', 'webp', 'gif'];
+		for (let i = 0; i < images.length; i++) {
+			const url = imageUrl(images[i]);
+			const res = await worker.fetch(`/?url=${encodeURI(url)}&t=${time}`, { headers: { accept: 'image/webp,image/jpeg,image/png' } });
+			expect(res.status).toBe(200);
+			expect(Object.fromEntries(res.headers.entries())).toMatchObject({ 'content-type': `image/${types[i]}`, 'cf-cache-status': 'HIT' });
+		}
 	});
-	test('jpeg', async () => {
+	test('not webp', async () => {
 		const { worker, time } = await property;
-		const url = 'https://raw.githubusercontent.com/node-libraries/wasm-image-optimization/master/images/test01.jpg';
-		const res = await worker.fetch(`/?url=${encodeURI(url)}&t=${time}`, { headers: { accept: 'image/jpeg,image/png' } });
-		expect(res.status).toBe(200);
-		expect(Object.fromEntries(res.headers.entries())).toMatchObject({ 'content-type': 'image/jpeg' });
-		expect(res.headers.get('cf-cache-status')).toBeNull();
-		console.log(Array.from(res.headers.entries()));
+		const types = ['png', 'jpeg', 'png', 'gif'];
+		for (let i = 0; i < images.length; i++) {
+			const url = imageUrl(images[i]);
+			const res = await worker.fetch(`/?url=${encodeURI(url)}&t=${time}`, { headers: { accept: 'image/jpeg,image/png' } });
+			expect(res.status).toBe(200);
+			expect(Object.fromEntries(res.headers.entries())).toMatchObject({ 'content-type': `image/${types[i]}` });
+			expect(res.headers.get('cf-cache-status')).toBeNull();
+		}
 	});
-	test('jpeg(cache)', async () => {
+	test('not webp(cache)', async () => {
 		const { worker, time } = await property;
-		const url = 'https://raw.githubusercontent.com/node-libraries/wasm-image-optimization/master/images/test01.jpg';
-		const res = await worker.fetch(`/?url=${encodeURI(url)}&t=${time}`, { headers: { accept: 'image/jpeg,image/png' } });
-		expect(res.status).toBe(200);
-		expect(Object.fromEntries(res.headers.entries())).toMatchObject({ 'content-type': 'image/jpeg', 'cf-cache-status': 'HIT' });
-		console.log(Array.from(res.headers.entries()));
+		const types = ['png', 'jpeg', 'png', 'gif'];
+		for (let i = 0; i < images.length; i++) {
+			const url = imageUrl(images[i]);
+			const res = await worker.fetch(`/?url=${encodeURI(url)}&t=${time}`, { headers: { accept: 'image/jpeg,image/png' } });
+			expect(res.status).toBe(200);
+			expect(Object.fromEntries(res.headers.entries())).toMatchObject({ 'content-type': `image/${types[i]}`, 'cf-cache-status': 'HIT' });
+		}
 	});
 });
